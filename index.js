ff432ec6f30fdd2388e15f6409e08323a9da3b5f
MightySenpai <49077805+MightySenpai@users.noreply.github.com>
2019-11-16 23:21:33 +0200

    Etch a sketch

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-const body = document.querySelector('body');
+const root = document.querySelector('#root');
 const createBoard = (dim) => {
-    const container = document.createElement('div');
-    container.id = "container"
-    container.classList.add('container');
-    container.addEventListener('mouseover', (event) => {
-        event.target.style.backgroundColor = 'red'
-        setTimeout(() => {
-            event.target.style.backgroundColor = "white"
-        }, 100);
-    })
+    const board = document.createElement('div');
+    board.id = "container"
+    board.classList.add('container');
+
     for (let i = 0; i < dim; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
         for (let j = 0; j < dim; j++) {
             const square = document.createElement('div');
             square.classList.add('square');
+            square.addEventListener('mouseover', changeColor)
             row.appendChild(square);
         }
-        container.appendChild(row);
+        board.appendChild(row);
     }
     const oldContainer = document.getElementById('container');
-    body.replaceChild(container, oldContainer);
+    root.replaceChild(board, oldContainer);
 }
 button.addEventListener('click', (event) => {
     const dim = document.getElementById('newGrid').value;
     createBoard(dim);
 })
-
-
-
+const createRandomColor = () => `rgb(${Math.random()*255})`
+const changeColor = (event) => {
+    event.target.style.backgroundColor = rgbColor();
+    event.target.removeEventListener('mouseover', changeColor)
+    event.target.addEventListener('mouseover', darkenColor)
+    // setTimeout(() => {
+    //     event.target.style.backgroundColor = "white"
+    // }, 100);
+}
+const rgbColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16);
+const darkenColor = (event) => {
+    console.log('hi im from darken')
+    event.target.style.backgroundColor = '#' + "000000";
+}
 createBoard(16)
